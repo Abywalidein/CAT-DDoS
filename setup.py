96b670ba45020c45fa1f392d13439cb591079dca
Abywalidein <mfath6961@gmail.com>
2024-12-12 19:47:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+import os
+c = input("Choose your environment: [0] pip / [1] pip3 : ")
 
+if c == "0":
+    os.system("apt-get install python")
+    os.system("apt-get install python2")
+    os.system("pip install fade")
+elif c == "1":
+    os.system("apt-get install python")
+    os.system("apt-get install python2")
+    os.system("pip install fade")
+print("Done.")
